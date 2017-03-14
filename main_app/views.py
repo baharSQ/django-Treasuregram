851bdb89500d8da92da3f3ade3678558a296899a
bahareh <bahar.sadeqian@gmail.com>
2017-03-15 00:16:48 +0200

    dynamic data

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
 def index(request):
-	return HttpResponse('<h1>Hello Explorer!</h1>')
+	return render(request, 'index.html', {'treasures':treasures})
+class Treasure:
+	def __init__(self, name, value, material, location):
+		self.name = name
+		self.value = value
+		self.material = material
+		self.location = location
+treasures = [
+	Treasure ('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM"),
+	Treasure ("Fool",0,'Pyrite',"Fool Fall, co")		
+]
